9c2a28071886750f58b5442e13a344ac1efae5cc
Ivan Perez <ivan.perez@keera.co.uk>
2014-07-08 18:52:38 +0200

    Uses nicer syntax to define the function.

--- src/Data/String/Extra.hs
+++ src/Data/String/Extra.hs
@@ -5,8 +5,11 @@ module Data.String.Extra
 -- | Auxiliary string functions. I can't believe no module declares these
 -- FIXME: Check that no existing module declares these.
 trim :: String -> String
-trim s = reverse (trimBeginning (reverse (trimBeginning s)))
+trim = trimEnd . trimBeginning
 
 trimBeginning :: String -> String
-trimBeginning (' ':xs) = trimBeginning xs
-trimBeginning xs       = xs
+trimBeginning = dropWhile (== ' ')
+
+-- FIXME: Use dropWhileEnd from 4.5.0.0
+trimEnd :: String -> String
+trimEnd = reverse . trimBeginning . reverse
