8d24b3bc8ed0b6457b1e245aaccd39e7f0b2ebb9
Myro Pavlenko <myro@keera.co.uk>
2020-02-26 19:22:14 +0000

    Remove dependency on glib. Refs #2.
    This package currently provides one operation that makes it depend on Glib.
    Although many hails applications need Gtk anyway, not all do, and it becomes
    harder to support other platforms (iOS, Android) if we have such dependency.
    
    This commit eliminates that operation, making it a comment, and removes
    the dependency from the cabal file.

--- src/Control/Exception/Extra.hs
+++ src/Control/Exception/Extra.hs
@@ -1,13 +1,28 @@
 module Control.Exception.Extra where
 
 import           GHC.Conc
-import           System.Glib.GError
 import qualified Control.Exception as E
 
 -- | Returns a given computation ignoring an exception
 anyway :: a -> E.SomeException -> a
 anyway f _ = f
 
+-- | Execute a computation using a handler to handle all exceptions. Note
+-- that this does not really handle all possible exceptions reported by GTK+.
+--
+-- A more comprehensive version would be:
+--
+-- @
+-- import System.Glib.GError   -- from glib package
+--
+-- handleAllExceptions :: IO () -> IO () -> IO ()
+-- handleAllExceptions handler op = do
+--     setUncaughtExceptionHandler (anyway handler)
+--     E.handle (anyway handler) $ handleGError (anywayG handler) op
+--   where
+--     anywayG :: IO a -> GError -> IO a
+--     anywayG x (GError _dom _code _msg) = x
+-- @
 handleExceptions :: IO a -> IO a -> IO a
 handleExceptions handler = E.handle (anyway handler)
 
@@ -16,12 +31,3 @@ handleExceptions handler = E.handle (anyway handler)
 trySeq :: [IO ()] -> IO ()
 trySeq []     = return ()
 trySeq (x:xs) = E.handle (anyway (trySeq xs)) x
-
--- | Handles any exception (apparently the default handle won't handle all)
-handleAllExceptions :: IO () -> IO () -> IO ()
-handleAllExceptions handler op = do
-  setUncaughtExceptionHandler (anyway handler)
-  E.handle (anyway handler) $ handleGError (anywayG handler) op
-
-anywayG :: IO a -> GError -> IO a
-anywayG x (GError _dom _code _msg) = x
